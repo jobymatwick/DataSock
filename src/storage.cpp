8d53528784fb7adce0566e7452ac1f914a1a2919
Joby Matwick <joby@matwick.ca>
2022-02-09 20:27:16 -0800

    Use shared SdFs object in format function

--- src/storage.cpp
+++ src/storage.cpp
@@ -15,28 +15,29 @@
 
 SdFs _sd;
 
+void _errorHalt();
+
 bool storage_format()
 {
-    _sd.end(); // End SD usage if started
+    _sd.cardBegin(SdioConfig(FIFO_SDIO));
 
-    SdCardFactory factory;
-    SdCard* card = factory.newCard(SdioConfig(FIFO_SDIO));
-    uint8_t code = card->errorCode();
-    if (!card || code)
+    if (!_sd.card() || _sd.card()->errorCode())
     {
-        Serial.printf("Failed to init SD card for format (%d)\r\n", code);
+        Serial.println("Failed to init SD card for format");
+        _errorHalt();
         return false;
     }
 
-    uint32_t sectors = card->sectorCount();
+    uint32_t sectors = _sd.card()->sectorCount();
     if (!sectors)
     {
         Serial.println("Failed to get sector count for format");
+        _errorHalt();
         return false;
     }
 
     Serial.printf("Found %.2f GB SD card\r\n", (sectors * 512E-8) / 10.0);
-    Serial.print("Starting erase... ....");
+    Serial.print("Starting erase...     ");
 
     // Erase all SD sectors
     uint32_t first_block = 0;
@@ -47,9 +48,10 @@ bool storage_format()
         last_block = (first_block + ERASE_SIZE > sectors)
             ? sectors - 1 : first_block + ERASE_SIZE - 1;
 
-        if (!card->erase(first_block, last_block))
+        if (!_sd.card()->erase(first_block, last_block))
         {
             Serial.printf("Failed to erase %d blocks at %d\r\n", last_block - first_block, first_block);
+            _errorHalt();
             return false;
         }
 
@@ -62,9 +64,10 @@ bool storage_format()
 
     // Verify erase
     uint8_t buf[512];
-    if (!card->readSector(0, buf))
+    if (!_sd.card()->readSector(0, buf))
     {
-        Serial.printf("Failed to read first block (%d)\r\n", card->errorCode());
+        Serial.println("Failed to read first block");
+        _errorHalt();
         return false;
     }
     Serial.printf("All blocks erased to 0x%02x\r\n", buf[0]);
@@ -72,12 +75,14 @@ bool storage_format()
     // Format as exFAT
     ExFatFormatter exFatFormatter;
     Serial.println("Starting format...");
-    if (!exFatFormatter.format(card, buf, &Serial))
+    if (!exFatFormatter.format(_sd.card(), buf, &Serial))
     {
         Serial.println("Formatting fail");
+        _errorHalt();
         return false;
     }
 
+    _sd.end();
     return true;
 }
 
@@ -89,4 +94,21 @@ bool storage_console(uint8_t argc, char* argv[])
     }
 
     return false;
+}
+
+void _errorHalt()
+{
+    if (!_sd.card())
+    {
+        Serial.println("Invalid SD config!");
+    }
+    else if (_sd.card()->errorCode() == SD_CARD_ERROR_CMD0)
+    {
+        Serial.println("No card, wrong chip select pin, or wiring error?");
+    }
+
+    Serial.printf("SD error code: 0x%02X, data: 0x%02X\r\n",
+                  _sd.card()->errorCode(), _sd.card()->errorData());
+
+    _sd.end();
 }
